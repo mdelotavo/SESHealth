5e0d05d8f6a859ad02cda96d543629fac23a289b
Matthew Tria Delotavo <matthew.t.delotavo@student.uts.edu.au>
2018-08-27 21:29:58 +1000

    HeartRateFragment.java: add intent to HeartRateMonitor.class
    - AndroidManifest.xml: add .Activities.HeartRateMonitor
    
    - AndroidManifest.xml: request camera permissions
    
    - heartbeat_view.xml: fix path to HeartbeatView

--- SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/HeartRateFragment.java
+++ SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/HeartRateFragment.java
@@ -1,12 +1,14 @@
 package team7.seshealthpatient.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import team7.seshealthpatient.Activities.HeartRateMonitor;
 import team7.seshealthpatient.R;
 
 /**
@@ -22,6 +24,8 @@ public class HeartRateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Intent intent = new Intent(getActivity(), HeartRateMonitor.class);
+        startActivity(intent);
         return inflater.inflate(R.layout.fragment_heart_rate, container, false);
     }
 
