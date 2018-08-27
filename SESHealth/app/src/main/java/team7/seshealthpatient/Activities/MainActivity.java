6ca1bd15fea397694c5d0261ec951001ac3d0044
Jahmilli <sebastian.southern@gmail.com>
2018-08-27 20:53:21 +1000

    reverting back to old camera api

--- SESHealth/app/src/main/java/team7/seshealthpatient/Activities/MainActivity.java
+++ SESHealth/app/src/main/java/team7/seshealthpatient/Activities/MainActivity.java
@@ -159,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
                                 break;
                             case R.id.nav_recordvideo:
                                 if (currentState != MenuStates.RECORD_VIDEO) {
-                                    ChangeFragment(new RecordVideoFragment(getApplicationContext()));
+                                    ChangeFragment(new RecordVideoFragment());
                                     currentState = MenuStates.RECORD_VIDEO;
                                 }
                                 break;
--- SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/RecordVideoFragment.java
+++ SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/RecordVideoFragment.java
@@ -37,31 +37,7 @@ public class RecordVideoFragment extends Fragment {
     @BindView(R.id.cameraBtn)
     Button cameraBtn;
 
-    @BindView(R.id.cameraTextureView)
-    TextureView textureView;
 
-    // Check state orientation of output image
-    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
-    static {
-        ORIENTATIONS.append(Surface.ROTATION_0, 90);
-        ORIENTATIONS.append(Surface.ROTATION_90, 0);
-        ORIENTATIONS.append(Surface.ROTATION_180, 270);
-        ORIENTATIONS.append(Surface.ROTATION_270, 180);
-    }
-
-    private String cameraId;
-    private CameraDevice cameraDevice;
-    private CameraCaptureSession cameraCaptureSession;
-    private CaptureRequest.Builder captureRequestBuilder;
-    private Size imageDimension;
-    private ImageReader imageReader;
-
-    // Save to file
-    private File file;
-    private static final int REQUEST_CAMERA_PERMISSION = 200;
-    private boolean mFlashSupported;
-    private Handler mBackgroundHandler;
-    private HandlerThread mBackgroundThread;
 
     public RecordVideoFragment() {
         // Required empty public constructor
@@ -92,39 +68,11 @@ public class RecordVideoFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        assert textureView != null;
-        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
-            @Override
-            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
-                openCamera();
-            }
-
-            @Override
-            public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
-
-            }
-
-            @Override
-            public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
-                return false;
-            }
-
-            @Override
-            public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
-
-            }
-        });
-
     }
 
     @OnClick(R.id.cameraBtn)
     public void cameraOnClick() {
-        if(cameraDevice == null)
-            return;
-        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        try {
-
-        }
+       
     }
 
     private void openCamera() {
