dc248dfbdc409f20d03381cb627ade76ca48a8c5
Jahmilli <sebastian.southern@gmail.com>
2018-08-28 15:50:33 +1000

    added check for camera permission to stop camera crashing

--- SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/RecordVideoFragment.java
+++ SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/RecordVideoFragment.java
@@ -1,19 +1,39 @@
 package team7.seshealthpatient.Fragments;
 
 
+import android.Manifest;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -26,6 +46,13 @@ import team7.seshealthpatient.R;
 public class RecordVideoFragment extends Fragment {
     private static String TAG = "RecordVideoFragment";
     private static final int  CAMERA_REQUEST_CODE = 5;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+
+    private static final String[] CAMERA_PERMISSION = {
+            Manifest.permission.CAMERA
+    };
+
 
     @BindView(R.id.cameraBtn)
     Button cameraBtn;
@@ -42,6 +69,8 @@ public class RecordVideoFragment extends Fragment {
         super.onCreate(savedInstanceState);
         // Note the use of getActivity() to reference the Activity holding this fragment
         getActivity().setTitle("Record Video");
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
     }
 
 
@@ -61,33 +90,90 @@ public class RecordVideoFragment extends Fragment {
 
     }
 
+    // During onClick event, the camera application will open up allowing users to record video
     @OnClick(R.id.cameraBtn)
     public void cameraOnClick() {
-
-        Intent cameraIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 8);
-        if(cameraIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-           Log.d(TAG, "OnClick: starting camera");
-           startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
-       } else {
-           Log.d(TAG, "Sending user back to home screen");
-           Intent intent = new Intent(getActivity(), MainActivity.class);
-           intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-           startActivity(intent);
-       }
+        if(checkPermissions(CAMERA_PERMISSION[0])){
+            Log.d(TAG, "onClick: starting camera");
+            Intent cameraIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+            cameraIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 8);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+        } else {
+            Toast.makeText(getActivity(),
+                    getString(R.string.cameraPermissionException),
+                    Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
     }
 
+    // This is triggered after the user presses 'ok' after recording video
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == CAMERA_REQUEST_CODE) {
             Uri videoUri = data.getData();
-            Log.d(TAG, "onActivityResult: done taking a photo");
+            Log.d(TAG, "onActivityResult: done taking a video");
             mVideoView.setVideoURI(videoUri);
             mVideoView.start();
+
+            String uri = getRealPathFromURI(videoUri);
+            InputStream stream = null;
+            try {
+                stream = new FileInputStream(new File(uri));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            // FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("Profile");
+
+            String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            String[] uriPath = uri.split("/[a-zA-z0-9]");
+            String uriString = uriPath[uriPath.length-1];
+
+            StorageReference videoRef = storageRef.child("Users/" + userId + "/videos/" + uriString);
+            UploadTask uploadTask = videoRef.putStream(stream);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Log.d(TAG, "An error occurred in uploading the file: " + exception);
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+                    Log.d(TAG, "Upload video succeeded: " + taskSnapshot.toString());
+                }
+            });
         }
     }
 
+    private String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(getActivity(), contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+        return result;
+    }
+
+    public boolean checkPermissions(String permission){
+        Log.d(TAG, "checkPermissions: checking permission: " + permission);
+        int permissionRequest = ActivityCompat.checkSelfPermission(getActivity(), permission);
+        if(permissionRequest != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "checkPermissions: \n Permission was not granted for: " + permission);
+            return false;
+        }
+        else{
+            Log.d(TAG, "checkPermissions: \n Permission was granted for: " + permission);
+            return true;
+        }
+    }
 
 
 }
