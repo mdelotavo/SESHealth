d2c36170260a0e4e28617e9df4d653fb5097801f
Jahmilli <sebastian.southern@gmail.com>
2018-09-01 15:16:42 +1000

    updated SendFileFragment UI

--- SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/SendFileFragment.java
+++ SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/SendFileFragment.java
@@ -1,12 +1,15 @@
 package team7.seshealthpatient.Fragments;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
 import team7.seshealthpatient.R;
 
 /**
@@ -14,17 +17,36 @@ import team7.seshealthpatient.R;
  */
 public class SendFileFragment extends Fragment {
 
-
     public SendFileFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Note the use of getActivity() to reference the Activity holding this fragment
+        getActivity().setTitle("Send file");
+        ProgressDialog progressDialog = new ProgressDialog(getActivity());
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send_file, container, false);
+        View v = inflater.inflate(R.layout.fragment_send_file, container, false);
+        ButterKnife.bind(this, v);
+
+        // Note how we are telling butter knife to bind during the on create view method
+        ButterKnife.bind(this, v);
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Now that the view has been created, we can use butter knife functionality
     }
 
 }
