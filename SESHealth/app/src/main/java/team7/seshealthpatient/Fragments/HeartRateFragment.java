38e06f6a8074397c72ac63fa7edcfbe4b92a5b2e
Matthew Tria Delotavo <matthew.t.delotavo@student.uts.edu.au>
2018-09-03 00:20:48 +1000

    HeartRateFragment.java: move inflater comment

--- SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/HeartRateFragment.java
+++ SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/HeartRateFragment.java
@@ -23,9 +23,9 @@ public class HeartRateFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         Intent intent = new Intent(getActivity(), HeartRateMonitor.class);
         startActivity(intent);
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_heart_rate, container, false);
     }
 
