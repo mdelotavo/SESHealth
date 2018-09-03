3a6affd7deea2a98046d6701330204a26c27f30b
Matthew Tria Delotavo <matthew.t.delotavo@student.uts.edu.au>
2018-09-03 20:28:07 +1000

    Add fragment_chat.xml

--- SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/ChatFragment.java
+++ SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/ChatFragment.java
@@ -1,6 +1,23 @@
 package team7.seshealthpatient.Fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import team7.seshealthpatient.R;
 
 public class ChatFragment extends Fragment {
+
+    public ChatFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
 }
