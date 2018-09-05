5b53de6029668362c3142dc32c9b50bf209a6abc
Matthew Tria Delotavo <matthew.t.delotavo@student.uts.edu.au>
2018-09-05 13:44:11 +1000

    ChatFragment.java: store messages in .child(uid).child("chat")

--- SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/ChatFragment.java
+++ SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/ChatFragment.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 
 import team7.seshealthpatient.R;
@@ -38,16 +39,22 @@ public class ChatFragment extends Fragment {
         FloatingActionButton fab = v.findViewById(R.id.fab);
         final EditText input = v.findViewById(R.id.input);
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final String uid = user.getUid();
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseDatabase.getInstance()
                         .getReference()
+                        .child("Users")
+                        .child(uid)
+                        .child("chat")
                         .push()
                         .setValue(new ChatMessage(input.getText().toString(),
                                 FirebaseAuth.getInstance().getCurrentUser().getDisplayName())
                         );
-                toastMessage("sent");
+                toastMessage("Sent");
                 input.setText("");
             }
         });
