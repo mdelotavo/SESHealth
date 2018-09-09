eeeefd8745a588be2c2fbd171186b495258a29ae
zerrain <37678931+zerrain@users.noreply.github.com>
2018-09-09 17:31:55 +1000

    Revamped settings and made a few smaller tweaks  (#20)
    * Revamped Settings fragment and made some other tweaks
    
    - Maps fragment now sets the toolbar title
    - Removed the stuttering from selecting an item in the nav drawer
    - Revamped the settings menu
    - Added a custom dialog for changing password in the settings fragment
    - Updated styles.xml
    - Minor ui tweaks to PatientInformationFragment and EditInfoActivity
    - Adjusted navigation drawer to remove stutter when selecting a fragment
    
    * Fixed EditUserInfo landscape mode
    
    * Switched about and logout in preferences

--- SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/SettingsFragment.java
+++ SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/SettingsFragment.java
@@ -1,56 +1,46 @@
 package team7.seshealthpatient.Fragments;
 
-import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.support.annotation.NonNull;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
+import team7.seshealthpatient.Activities.EditInfoActivity;
+import team7.seshealthpatient.Activities.LoginActivity;
 import team7.seshealthpatient.Activities.MainActivity;
 import team7.seshealthpatient.R;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragment {
+
     private static String TAG = "SettingsFragment";
-    private ProgressDialog progressDialog;
-
-    @BindView(R.id.settingsCurrentPwET)
-    EditText currentPasswordET;
-
-    @BindView(R.id.settingsNewPwET)
-    EditText newPasswordET;
-
-    @BindView(R.id.settingsNewRetypedPwET)
-    EditText newRetypedPasswordET;
+    private AlertDialog.Builder logoutAlertBuilder;
+    private AlertDialog logoutAlert;
+    private AlertDialog.Builder aboutAlertBuilder;
+    private AlertDialog aboutAlert;
 
     public SettingsFragment() {
+
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+        setOnPreferenceClickListeners();
+        initAlertBuilders();
 
         // Note the use of getActivity() to reference the Activity holding this fragment
         getActivity().setTitle("Settings");
-        progressDialog = new ProgressDialog(getActivity());
     }
 
     @Override
@@ -65,94 +55,77 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // Now that the view has been created, we can use butter knife functionality
     }
 
-    private boolean isValidPassword(CharSequence target) {
-        return target.length() >= 6;
-    }
-
-    @OnClick(R.id.settingsSaveBtn)
-    public void saveNewPassword() {
-        String currentPassword = currentPasswordET.getText().toString().trim();
-        final String newPassword = newPasswordET.getText().toString().trim();
-        String newRetypedPassword = newRetypedPasswordET.getText().toString().trim();
-        final FirebaseAuth mAuth = ((MainActivity)getActivity()).getFirebaseAuth();
-        final FirebaseUser mUser = mAuth.getCurrentUser();
-
-        // Alert users that authenticated with Google that they cannot change their password from this application
-        try {
-            if (mUser.getProviders().contains("google.com")) {
-                Toast.makeText(getActivity(), "You cannot change your Google password from here", Toast.LENGTH_LONG).show();
-                return;
+    public void setOnPreferenceClickListeners() {
+        Preference editUserInfoPreference = findPreference("editUserInfoPreference");
+        editUserInfoPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivity(new Intent(getContext(), EditInfoActivity.class));
+                return false;
             }
-        } catch(Exception e) {
-            Log.d(TAG, e.toString());
-        }
-        if(!isValidPassword(currentPassword) || !isValidPassword(newPassword) || !isValidPassword(newRetypedPassword)) {
-            Toast.makeText(getActivity(), "Please fill in all fields with 6 or more characters", Toast.LENGTH_SHORT).show();
-            return;
-        }
+        });
 
-        if(!newPassword.equals(newRetypedPassword)) {
-            Log.d(TAG, "Passwords do not match");
-            Toast.makeText(getActivity(), "Your new and retyped passwords do not match", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        Log.d(TAG, mUser.getProviderId() + "\n" + mUser.getProviderData().toString() + "\n" + mUser.getProviders().toString());
+        Preference logOutPreference = findPreference("logOutPreference");
+        logOutPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                logoutAlert.show();
+                return false;
+            }
+        });
 
-        // Get auth credentials from the user for re-authentication. The example below shows
-        // email and password credentials but there are multiple possible providers,
-        // such as GoogleAuthProvider or FacebookAuthProvider.
-                AuthCredential credential = EmailAuthProvider
-                        .getCredential(mUser.getEmail(), currentPassword);
+        Preference aboutPreference = findPreference("aboutPreference");
+        aboutPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                aboutAlert.show();
+                return false;
+            }
+        });
+    }
 
-        progressDialog.setMessage("Setting new password, please wait...");
-        progressDialog.show();
-        // Prompt the user to re-provide their sign-in credentials
-            mUser.reauthenticate(credential)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
+    public void initAlertBuilders() {
+        logoutAlertBuilder = new AlertDialog.Builder(getContext());
+        logoutAlertBuilder.setMessage("Are you sure you want to log out?");
+        logoutAlertBuilder.setCancelable(true);
+
+        logoutAlertBuilder.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
                     @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()) {
-                            Log.d(TAG, "User successfully re-authenticated");
-                            updatePassword(mUser, newPassword);
-                        } else {
-                            Log.d(TAG, "User was unable to re-authenticate. " + task.getException());
-                            progressDialog.dismiss();
-                            if(task.getException().toString().startsWith("com.google.firebase.auth.FirebaseAuthInvalidCredentialsException")) {
-                                Toast.makeText(getActivity(), "Your current password is incorrect",
-                                        Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(getActivity(), "An error occurred, please try again",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        }
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(new Intent(getContext(), LoginActivity.class));
+                        getActivity().finish();
                     }
                 });
-    }
 
-    private void updatePassword(FirebaseUser user, String password) {
-        final FirebaseUser mUser = user;
-        mUser.updatePassword(password)
-            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    progressDialog.dismiss();
-                    if (task.isSuccessful()) {
-                        Log.d(TAG, mUser.getEmail() + " has successfully updated their password");
-                        Toast.makeText(getActivity(), "Your new password has been set",
-                                Toast.LENGTH_SHORT).show();
-                    } else {
-                        Log.d(TAG, "An error occurred during updating the password: " + task.getException());
-                        Toast.makeText(getActivity(), "An error occurred, please try again",
-                                Toast.LENGTH_SHORT).show();
+        logoutAlertBuilder.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
                     }
-                }
-            });
+                });
+
+        logoutAlert = logoutAlertBuilder.create();
+
+        aboutAlertBuilder = new AlertDialog.Builder(getContext());
+        aboutAlertBuilder.setMessage("This application was developed by Software Engineering Studio 1B Team 7");
+        aboutAlertBuilder.setCancelable(true);
+
+        aboutAlertBuilder.setPositiveButton(
+                "Ok",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        aboutAlert = aboutAlertBuilder.create();
     }
-
-
-
-
 }
