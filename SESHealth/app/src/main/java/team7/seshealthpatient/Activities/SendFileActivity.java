e36609dbf313dcd2ae13c3a9f4637a32ed218c3c
Xuan Hieu <12824658@student.uts.edu.au>
2018-09-21 18:26:54 +1000

    -Added "Upload" button -Added "Select file" button -Added upload process bar

--- SESHealth/app/src/main/java/team7/seshealthpatient/Activities/SendFileActivity.java
+++ SESHealth/app/src/main/java/team7/seshealthpatient/Activities/SendFileActivity.java
@@ -20,6 +20,7 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -27,6 +28,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -34,12 +36,15 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
@@ -88,6 +93,10 @@ public class SendFileActivity extends AppCompatActivity {
     private Uri videoUri = null;
     private int heartBeatAvg = 0;
 
+    Button selectFile, upload;
+    TextView notification;
+    Uri pdfUri;
+
     private static final String[] CAMERA_PERMISSION = {
             Manifest.permission.CAMERA,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -133,12 +142,83 @@ public class SendFileActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
 
         progressDialog = new ProgressDialog(this);
+        ///////////////////////
         storage = FirebaseStorage.getInstance();
         storageRef = storage.getReference();
         database = FirebaseDatabase.getInstance();
         reference = database.getReference("Users").child(mUser.getUid());
+        storage = FirebaseStorage.getInstance();
+        database = FirebaseDatabase.getInstance();
+        selectFile = findViewById(R.id.selectFile);
+        upload = findViewById(R.id.upload);
+        notification = findViewById(R.id.notification);
+        selectFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(SendFileActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    selectPdf();
+                }
+                else
+                    ActivityCompat.requestPermissions(SendFileActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},9);
+            }
+        });/////////////////
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(pdfUri!=null)
+                    uploadFile(pdfUri);
+                else
+                    Toast.makeText(SendFileActivity.this,"Select a File",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void uploadFile(Uri pdfUri){
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setTitle("Uploading file...");
+        progressDialog.setProgress(0);
+        progressDialog.show();
+        final String fileName=System.currentTimeMillis()+"";
+        StorageReference storageReference=storage.getReference();
+        storageReference.child("Uploads").child(fileName).putFile(pdfUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        String url=taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                        DatabaseReference reference=database.getReference();
+                        reference.child(fileName).setValue(url).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful())
+                                    Toast.makeText(SendFileActivity.this,"File succesfully uploaded",Toast.LENGTH_SHORT).show();
+                                else
+                                    Toast.makeText(SendFileActivity.this,"File NOT succesfully uploaded",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SendFileActivity.this,"File NOT succesfully uploaded",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                int currentProgress=(int)(100*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                progressDialog.setProgress(currentProgress);
+            }
+        }); //////////////
     }
 
+    private void selectPdf(){
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 86);
+    }
+
+
+    //////
     // During onClick event, the camera application will open up allowing users to record video
     public void recordVideo() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -297,6 +377,14 @@ public class SendFileActivity extends AppCompatActivity {
                 Log.d(TAG, "An error occurred when getting the heartbeat");
             }
         }
+
+        if(requestCode==86 && resultCode==RESULT_OK && data!=null){
+            pdfUri=data.getData();
+            notification.setText("A file is selected: "+ data.getData().getLastPathSegment());
+        }
+        else{
+            Toast.makeText(SendFileActivity.this,"Please select a file",Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void uploadToFirebase(final StorageReference ref, UploadTask uploadTask) {
@@ -451,6 +539,11 @@ public class SendFileActivity extends AppCompatActivity {
                 }
                 break;
         }
+        if(requestCode==9 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            selectPdf();
+        }
+        else
+            Toast.makeText(SendFileActivity.this,"Please provide permission...",Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -466,4 +559,3 @@ public class SendFileActivity extends AppCompatActivity {
         return true;
     }
 }
-//test
