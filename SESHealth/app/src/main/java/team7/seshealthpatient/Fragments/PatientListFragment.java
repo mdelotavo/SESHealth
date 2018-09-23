02a879e31cfaf525da5a6e2f37fe92b71de4f507
Matthew Tria Delotavo <matthew.t.delotavo@student.uts.edu.au>
2018-09-23 22:58:59 +1000

    Grab list of users from Firebase RTDB

--- SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/PatientListFragment.java
+++ SESHealth/app/src/main/java/team7/seshealthpatient/Fragments/PatientListFragment.java
@@ -2,9 +2,28 @@ package team7.seshealthpatient.Fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import team7.seshealthpatient.R;
 
 public class PatientListFragment extends Fragment {
+
+    FirebaseUser user;
+    String uid, name, doctorKey;
+    boolean isDoctor = false;
+    ListView listOfPatients;
 
     public PatientListFragment() {
 
@@ -15,5 +34,49 @@ public class PatientListFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         getActivity().setTitle("Patient List");
+
+        doctorKey = "1234";
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        uid = user.getUid();
+
+        FirebaseDatabase.getInstance().getReference().child("Users").child(uid).child("Profile").child("name")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        name = (String) dataSnapshot.getValue();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_patient_list, container, false);
+
+        listOfPatients = v.findViewById(R.id.list_of_patients);
+
+        FirebaseDatabase.getInstance().getReference().child("Users")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot child : dataSnapshot.getChildren()) {
+                            String key = child.getKey();
+                            String patient = (child.child("Profile").child("name").getValue() != null)
+                                    ? child.child("Profile").child("name").getValue().toString() : null;
+                            System.out.println(key + " " + patient);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+        return v;
     }
 }
